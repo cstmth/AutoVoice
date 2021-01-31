d7a1ffb700bfdc31333e444f83727d92447d16f0
Carl-Luis Dressler <ich@carldressler.de>
2021-01-31 22:37:49 +0100

    added error message if no valid start argument is provided, modified shadowJar task slightly

--- src/main/java/de/carldressler/autovoice/Bot.java
+++ src/main/java/de/carldressler/autovoice/Bot.java
@@ -1,21 +1,25 @@
 package de.carldressler.autovoice;
 
-import de.carldressler.autovoice.utilities.database.DB;
 import de.carldressler.autovoice.listeners.ChannelEventListener;
 import de.carldressler.autovoice.listeners.CommandHandler;
 import de.carldressler.autovoice.listeners.VoiceEventListener;
 import de.carldressler.autovoice.utilities.Constants;
+import de.carldressler.autovoice.utilities.database.DB;
 import de.carldressler.autovoice.utilities.startup.ConfigAccessor;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
+import java.util.Set;
 
 public class Bot {
     public static JDA jda;
     public static ConfigAccessor configAccessor;
     public static boolean runsInProdMode;
+    private static final Logger logger = LoggerFactory.getLogger("Bot");
 
     public static void main(String[] args) throws LoginException, InterruptedException {
         setProdMode(args);
@@ -35,7 +39,11 @@ public class Bot {
     }
 
     static void setProdMode(String[] args) {
-        runsInProdMode = args.length > 1 || args[0].equals("prod");
+        if (args.length < 1 || !Set.of("prod", "dev").contains(args[0])) {
+            logger.error("No or invalid first argument. Set first argument to either 'dev' or 'prod'");
+            System.exit(1);
+        }
+        runsInProdMode = args[0].equals("prod");
     }
 
     public static String getToken() {
